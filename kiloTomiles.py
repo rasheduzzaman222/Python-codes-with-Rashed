2eb34e750c21a6e94507961950909cad869ae894
rasheduzzaman222 <rasheduzzaman788@gmail.com>
2025-08-19 23:31:44 +0600

    First Commit

---
+++ kiloTomiles.py
@@ -0,0 +1,7 @@
+kilometers=float(input("Enter distance in kilometers: "))
+miles=kilometers*0.621371
+print("After converting kilometers to miles is:", round(miles, 2))
+
+Miles=float(input("Enter distance in miles: "))
+kilometers=Miles/0.621371  
+print("After converting miles to kilometers is:", round(kilometers, 2))
