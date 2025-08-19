c7506fb5ea56d3605c6dda361105bebade3ecac4
rasheduzzaman222 <rasheduzzaman788@gmail.com>
2025-08-20 00:18:32 +0600

    First commit

---
+++ LargestNumber.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first value: "))
+num2=int(input("Enter the second value: "))
+num3=int(input("Enter the third value: "))
+
+max_number = max(num1, num2, num3)
+print("The largest number is:", max_number)
