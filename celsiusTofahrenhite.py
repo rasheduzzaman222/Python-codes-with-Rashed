0358d0d0b521299727c6dcb1f7eb9df44f01d762
rasheduzzaman222 <rasheduzzaman788@gmail.com>
2025-08-19 23:44:27 +0600

    First commit

---
+++ celsiusTofahrenhite.py
@@ -0,0 +1,7 @@
+celsius=float(input("Enter temperature in celsius: "))
+Fahrenhite=(celsius*1.8)+32
+print(celsius, "degree celsius to fahrenhite is:", round(Fahrenhite))
+
+Fahrenhite=float(input("Enter temperature in fahrenhite: "))
+celsius=(Fahrenhite-32)/1.8
+print(Fahrenhite, "degree fahrenhite to celsius is:", round(celsius, 2))
---
+++ grade.py
@@ -0,0 +1,4 @@
+grade=int(input("Enter your mark: "))
+print("Your mark is: ", grade)
+result = "Pass" if grade >= 40 else "Fail"
+print("Result: ", result)
