aa88543b7e172323dee597f3a49d3acc2a6fc0c5
rasheduzzaman222 <rasheduzzaman788@gmail.com>
2025-08-19 08:44:27 +0600

    Update HelloPython.py with new changes

--- HelloPython.py
+++ HelloPython.py
@@ -1,2 +1,3 @@
-print('Hello Python!')
-print('Welcome to the world of Python programming!')
+print('Hello Python')
+print('Welcome to the world of Python programming')
+print('Let\'s start coding')
