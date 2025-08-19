9895d055a9befb783536154f808ef2f1264766f3
rasheduzzaman222 <rasheduzzaman788@gmail.com>
2025-08-19 14:30:39 +0600

    swap done3

---
+++ swapValue.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first value for swap: "))
+num2=int(input("Enter the second value of the swap: "))
+temp=0
+temp=num1
+num1=num2
+num2=temp
+print("After swapping, the first value is:", num1)
+print("After swapping, the second value is:", num2)
